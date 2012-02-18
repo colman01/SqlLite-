f7cce0795c6ce277af668984439aadeb1b15b4f5
colman marcus-quinn <colman01@gmail.com>
2012-02-18 14:33:36 +0100

    inital commit

--- SqlLiteSetup/AppDelegate.h
+++ SqlLiteSetup/AppDelegate.h
@@ -7,10 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
 @class ViewController;
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate> {
+    // SQLITE database
+    sqlite3 *database;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 
--- SqlLiteSetup/AppDelegate.m
+++ SqlLiteSetup/AppDelegate.m
@@ -75,4 +75,63 @@
      */
 }
 
+
+
+#pragma mark -
+#pragma mark - Database methods
+// Creates a writable copy of the bundled default database in the application Documents directory.
+- (void)createEditableCopyOfDatabaseIfNeeded {
+    // First, test for existence.
+    BOOL           success;
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError       *error;
+    NSArray       *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString      *documentsDirectory = [paths objectAtIndex:0];
+    NSString      *writableDBPath = [documentsDirectory stringByAppendingPathComponent:@"companyName.sqlite"];
+	
+    success = [fileManager fileExistsAtPath:writableDBPath];
+    
+	if (success) return;
+    
+	// The writable database does not exist, so copy the default to the appropriate location.
+    NSString *defaultDBPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"companyName.sqlite"];
+    
+	success = [fileManager copyItemAtPath:defaultDBPath toPath:writableDBPath error:&error];
+    if (!success) {
+        NSAssert1(0, @"Failed to create writable database file with message '%@'.", [error localizedDescription]);
+    }
+}
+
+// Open the database connection and retrieve minimal information for all objects.
+- (void)initializeDatabase {	
+	
+    // The database is stored in the application bundle. 
+    NSArray  *paths				 = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *path				 = [documentsDirectory stringByAppendingPathComponent:@"companyName.sqlite"];
+    
+    // Open the database
+	// Get searchs history
+    if (sqlite3_open([path UTF8String], &database) == SQLITE_OK) {
+        sqlite3_stmt *statement;
+		// Get the primary key
+		const char *sql = "SELECT id FROM favorite ORDER BY id DESC";
+		if (sqlite3_prepare_v2(database, sql, -1, &statement, NULL) == SQLITE_OK) {
+			while (sqlite3_step(statement) == SQLITE_ROW) {
+				int pk = sqlite3_column_int(statement, 0);
+			}
+		}
+		
+        // "Finalize" the statement - releases the resources associated with the statement.
+        sqlite3_finalize(statement);
+    } else {
+        // Even though the open failed, call close to properly clean up resources.
+        sqlite3_close(database);
+        NSAssert1(0, @"Failed to open database with message '%s'.", sqlite3_errmsg(database));
+        // Additional error handling, as appropriate...
+    }
+}
+
+
+
 @end
