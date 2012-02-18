a23a32b72953fabbe96d496041a41613f7e288af
colman marcus-quinn <colman01@gmail.com>
2012-02-18 14:23:31 +0100

    Initial Commit

---
+++ SqlLiteSetup/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SqlLiteSetup
+//
+//  Created by Colman Marcus-Quinn on 18.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
